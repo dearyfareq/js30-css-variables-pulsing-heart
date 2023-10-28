58a6ff4ff2bc1c0999d3acee1e7884f5a39f3218
Diyari <dearyfareq15@gmail.com>
2023-10-28 20:23:34 +0300

    added comments for future reivew

--- css-variables.js
+++ css-variables.js
@@ -1,10 +1,13 @@
+// capture the input element
 const input = document.querySelector("input");
 
 function update() {
+  // take the root, style, set property of --(variable name), we used the element naming, matching the css variable name to target it
   document.documentElement.style.setProperty(`--${this.name}`, this.value);
   console.log(this.name);
 }
 
+// listen to ANY form of change, be it moving,mouse down and up
 input.addEventListener("mousemove", update);
 input.addEventListener("mousedown", update);
 input.addEventListener("mouseup", update);
